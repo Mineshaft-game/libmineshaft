7f61f096819ba64489deff660474ad982cac86cf
leha-code <pezleha@gmail.com>
2021-10-18 19:29:19 -0400

    I WILL do everything else rn just gonna work on Mineshaft

---
+++ libmineshaft/__main__.py
@@ -0,0 +1,4 @@
+import .shell
+
+
+#print("libmineshaft does not provide the interactive Mineshaft shell at this version ")
--- libmineshaft/main.py
+++
@@ -1,1 +0,0 @@
-print("libmineshaft does not provide the interactive Mineshaft shell at this version ")

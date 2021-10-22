d4ecbe5d729ec37a9ff06404e170b7a592a525ce
leha-code <pezleha@gmail.com>
2021-10-21 21:39:09 -0400

    Add cobblestone

--- libmineshaft/blocks.py
+++ libmineshaft/blocks.py
@@ -25,5 +25,7 @@ dirt = Block(1,  ["assets",  "images",  "blocks",  "dirt.png"],
              resistance=0,  name="Dirt",  falls=False,  breaktime=2)
 stone = Block(2,  ["assets",  "images",  "blocks",  "stone.png"], 
              resistance = 10,  name="Stone",  falls=False,  breaktime=15)
+cobblestone = Block(3,  ["assets", "images",  "blocks",  "cobblestone.png"], 
+             resistance = 10,  name = "Cobblestone",  falls=False,  breaktime=15)
 
 BLOCKS = {0 : air,  1 : dirt,  2 : stone}

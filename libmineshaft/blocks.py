638bb20c3bded195b919b74995b95f1cb1553e7f
leha-code <pezleha@gmail.com>
2021-10-22 20:46:05 -0400

    add multiple block classes for easier identification

--- libmineshaft/blocks.py
+++ libmineshaft/blocks.py
@@ -18,14 +18,50 @@ class Block:
         self.falls = falls
         self.breaktime = breaktime
 
+class NoIDBlock(Block):
+    def __init__(self, 
+                        image, 
+                        resistance=2, 
+                        name="No ID Block", 
+                        unbreakable=False, 
+                        falls=False, 
+                        breaktime=10):
+        self.image = os.path.join(image)
+        self.resistance = resistance
+        self.name = name
+        self.unbreakable = unbreakable
+        self.falls = falls
+        self.breaktime = breaktime
+
+
+class MultipleIDBlock(Block):
+    def __init__(self, 
+                        blocks):
+        if len(self.blocks) > 15:
+           raise IndexError("There is too much block IDs.\nMore block ID slots may be added in the future") 
+        self.blocks = blocks
+        self.default = self.blocks[0]
+        
+    
+
 
 air = Block(0,  image=False,
             resistance=-1,  name="Air",  falls=False,  breaktime=-1)
-dirt = Block(1,  ["assets",  "images",  "blocks",  "dirt.png"],
+stone = MultipleIDBlock([
+                                        NoIDBlock(["assets",  "images",  "blocks",  "stone.png"],
+                                                  resistance=10,  name="Stone",  falls=False,  breaktime=15)
+                                                  
+                                    ])
+
+grass = Block(2,  ["assets",  "images",  "blocks",  "grass.png"],
+              resistance=0,  name="Grass Block",  falls=False,  breaktime=2)
+
+dirt = Block(3,  ["assets",  "images",  "blocks",  "dirt.png"],
              resistance=0,  name="Dirt",  falls=False,  breaktime=2)
-stone = Block(2,  ["assets",  "images",  "blocks",  "stone.png"],
-              resistance=10,  name="Stone",  falls=False,  breaktime=15)
-cobblestone = Block(3,  ["assets", "images",  "blocks",  "cobblestone.png"],
+
+cobblestone = Block(4,  ["assets", "images",  "blocks",  "cobblestone.png"],
                     resistance=10,  name="Cobblestone",  falls=False,  breaktime=15)
+
+
 
 BLOCKS = {0: air,  1: dirt,  2: stone,  3: cobblestone}

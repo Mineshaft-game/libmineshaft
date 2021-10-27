2744f60481a91fa7ebe9169d5fb466da3311a029
leha-code <pezleha@gmail.com>
2021-10-26 21:14:20 -0400

    Add item durability and combat item classes:

--- libmineshaft/items.py
+++ libmineshaft/items.py
@@ -2,7 +2,33 @@ import os
 
 
 class Item:
-    def __init__(id, name, texture):
+    def __init__(self, id, name, texture):
         self.id = id
         self.name = name
         self.texture = os.path.join(texture)
+
+class DurabilityItem(Item):
+    def __init__(self, id, name, texture, durability):
+        self.id = id
+        self.name = name
+        self.texture = os.path.join(texture)
+        self.durability = durability
+
+    def use_durability(self):
+        '''
+        Decreases durability of the item.
+        Should be called in use()
+        '''
+        self.durability -= 1
+
+class CombatItem(DurabilityItem):
+    def __init__(self, id, name, texture, durability, damage):
+        self.id = id
+        self.name = name
+        self.texture = os.path.join(texture)
+        self.durability = durability
+        self.damage = damage
+
+    def attack(self, victim):
+        victim.health -= damage
+        self.use_durability()

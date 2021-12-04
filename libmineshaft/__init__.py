724ce79acb00e55fab5e0e971b9a487002c0adde
leha-code <pezleha@gmail.com>
2021-12-04 17:46:19 -0400

    v0.1.6rc-2

--- libmineshaft/__init__.py
+++ libmineshaft/__init__.py
@@ -3,7 +3,7 @@ libmineshaft
 ~~~~~~~~~~
 This module contains the metadata: The version, author name, and the credits.
 """
-__version__ = "0.1.6rc-1"
+__version__ = "0.1.6rc-2"
 __author__ = "Double Fractal Game Studios"
 __credits__ = """Double Fractal Game Studios team:
 *   Mayu Sakurai
